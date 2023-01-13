8cd9a188a4c2646a1d8488c82e827e37d6837030
Mtsumi <mtsumijr@gmail.com>
2023-01-13 13:48:14 +0300

    Py lint errors

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 ''' wait random module.
 '''
+
+
 import asyncio
 import random
+
 
 async def wait_random(max_delay: int = 10) -> float:
     '''Waits for a random number of seconds.
